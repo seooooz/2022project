8d9b400324e3afd0e079d6712338b9fa13650316
ju <107034875+seooooz@users.noreply.github.com>
2022-07-28 11:21:35 +0900

    서주test

--- 2022project/src/Test1.java
+++ 2022project/src/Test1.java
@@ -1,9 +1,10 @@
 
-public class Test {
+public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("º≠¡÷");
+		System.out.println("1");
+		System.out.println("1");
 	}
 
 }
