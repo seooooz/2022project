651a881c1f3058aa31c824ebc159ec3cad48ed2d
choye07 <forge07@naver.com>
2022-07-28 11:24:27 +0900

    예진 테스트

--- 2022project/src/Test.java
+++ 2022project/src/Test.java
@@ -3,8 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi");
-		System.out.println("hi");
+		System.out.println("¿¹Áø");
+		
 	}
 
 }
