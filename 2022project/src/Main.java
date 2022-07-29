c3a023523d7ab1ccda5b8629fed9408c0a6f2c7d
ju <107034875+seooooz@users.noreply.github.com>
2022-07-29 14:52:04 +0900

    한글

--- 2022project/src/Main.java
+++ 2022project/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("가나다라마바사아자자자자자");
+		System.out.println("김서주");
+		
 	}
 
 }
