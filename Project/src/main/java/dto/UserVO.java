2842319dc0d995ae3383e6a45229173ee3e58a87
potato <kmn0848@naver.com>
2022-09-14 12:03:41 +0900

    UserVo 생성

---
+++ Project/src/main/java/dto/UserVO.java
@@ -0,0 +1,26 @@
+package dto;
+
+public class UserVO {
+	private String name;
+	private String id;
+	private String pass;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+}
